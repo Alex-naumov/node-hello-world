1e98fdb019b8756f02a9d326ab67733f971d191f
Alex-naumov <56648843+Alex-naumov@users.noreply.github.com>
2021-08-11 04:40:40 +0300

    initial commit

--- src/handler.js
+++ src/handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Heloo World",
+        message: "Hello World",
         input: event,
       },
       null,
